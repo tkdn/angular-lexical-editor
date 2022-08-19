df0f62d64236977b5efb274c03a7f8bce5e1f193
tkdn <tkdnation@gmail.com>
2022-08-20 02:22:03 +0900

    My first lexical editor

--- src/app/components/editor/editor.component.ts
+++ src/app/components/editor/editor.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { registerPlainText } from '@lexical/plain-text';
+import { createEditor, LexicalEditor } from 'lexical';
 
 @Component({
   selector: 'lex-editor',
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.css']
 })
-export class EditorComponent implements OnInit {
+export class EditorComponent implements OnInit, AfterViewInit {
+  @ViewChild('editor')
+  private editableElement: ElementRef<HTMLElement | null>
 
-  constructor() { }
+  private editor: LexicalEditor
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.editor = createEditor();
+  }
+
+  ngAfterViewInit() {
+    this.editor.setRootElement(this.editableElement.nativeElement);
+    registerPlainText(this.editor);
   }
 
 }
