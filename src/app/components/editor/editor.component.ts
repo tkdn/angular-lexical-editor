ee298ed740c88c5bb10cca06c779a8f34a493903
tkdn <tkdnation@gmail.com>
2022-09-07 11:08:04 +0900

    Set Initial EditorState, and log EditorState snapshot

--- src/app/components/editor/editor.component.ts
+++ src/app/components/editor/editor.component.ts
@@ -1,20 +1,58 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { registerPlainText } from '@lexical/plain-text';
 import { createEditor, LexicalEditor } from 'lexical';
+
+const initialEditorState = JSON.stringify({
+  root: {
+    children: [
+      {
+        children: [
+          {
+            detail: 0,
+            format: 0,
+            mode: 'normal',
+            style: '',
+            text: '最初の状態',
+            type: 'text',
+            version: 1,
+          },
+        ],
+        direction: 'ltr',
+        format: '',
+        indent: 0,
+        type: 'paragraph',
+        version: 1,
+      },
+    ],
+    direction: 'ltr',
+    format: '',
+    indent: 0,
+    type: 'root',
+    version: 1,
+  },
+});
 
 @Component({
   selector: 'lex-editor',
   templateUrl: './editor.component.html',
-  styleUrls: ['./editor.component.css']
+  styleUrls: ['./editor.component.css'],
 })
 export class EditorComponent implements OnInit, AfterViewInit {
   @ViewChild('editor')
-  private editableElement: ElementRef<HTMLElement | null>
+  private editableElement: ElementRef<HTMLElement | null>;
 
-  private editor: LexicalEditor
+  private editor: LexicalEditor;
 
   ngOnInit() {
     this.editor = createEditor();
+    const parsedEditorState = this.editor.parseEditorState(initialEditorState);
+    this.editor.setEditorState(parsedEditorState);
   }
 
   ngAfterViewInit() {
@@ -22,4 +60,7 @@ export class EditorComponent implements OnInit, AfterViewInit {
     registerPlainText(this.editor);
   }
 
+  onButtonPress() {
+    console.log(JSON.stringify(this.editor.getEditorState()));
+  }
 }
